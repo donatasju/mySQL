6b80f5a4f3ca4c3d36e1c29a3e625a64eb0a36df
donatasju <donatas.jurkusl@gmail.com>
2019-04-16 13:04:24 +0300

    connection done

--- public_html/test.php
+++ public_html/test.php
@@ -4,11 +4,32 @@ require_once '../bootloader.php';
 
 $db = new \Core\Database\Connection([
     'host' => 'localhost',
-    'user' => 'root',   
+    'user' => 'root',
     'password' => '12345'
-]);
+        ]);
 
 $pdo = $db->getPDO();
-$pdo->exec("INSERT INTO `my_db`.`users`(`email`, `password`, `full_name`, `age`, `gender`, `photo`)" . 
-        "VALUES('donatas.jurkusl@gmail.com', '12345', 'Donatas Jurkus', '23', 'm', 'dick.jpg')");
+$query = $pdo->prepare("INSERT INTO `my_db`.`users`"
+        . "(`email`, `password`, `full_name`, `age`, `gender`, `photo`)" .
+        "VALUES(:email, :password, :full_name, :age, :gender, :photo)");
+
+$data = [
+    'email' => 'lohatronas@gmail.com',
+    'password' => 'qwer1234',
+    'full_name' => 'lohas lohauskas',
+    'age' => 12,
+    'gender' => 'f',
+    'photo' => 'loh.jpg'
+];
+
+$query->bindParam(':email', $data['email'], PDO::PARAM_STR);
+$query->bindParam(':password', $data['password'], PDO::PARAM_STR);
+$query->bindParam(':full_name', $data['full_name'], PDO::PARAM_STR);
+$query->bindParam(':age', $data['age'], PDO::PARAM_INT);
+$query->bindParam(':gender', $data['gender'], PDO::PARAM_STR);
+$query->bindParam(':photo', $data['photo'], PDO::PARAM_STR);
+
+$query->execute();
+
+
 
