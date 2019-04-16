cb47ff2384a3ccd315d1b63fe60ff4357954b2dd
donatasju <donatas.jurkusl@gmail.com>
2019-04-16 11:32:41 +0300

    connection done

--- core/database/Connection.php
+++ core/database/Connection.php
@@ -5,24 +5,27 @@ namespace Core\Database;
 use PDO;
 
 class Connection extends Core\Database\Abstracts\Connection {
+
     protected $creds;
-    
+
     public function __construct($creds) {
         $this->setCredentials($creds);
     }
 
     public function connect() {
-        try {
-            $this->pdo = new PDO(
-                    "mysql:host=$this->host", $this->user, $this->pass
-            );
+        if (!$this->pdo) {
+            try {
+                $this->pdo = new PDO(
+                        "mysql:host=$this->host", $this->user, $this->pass
+                );
 
-            if (DEBUG) {
-                $this->pdo->setAttribute(PDO::ATTR_EMULATE, PDO::ERRMODE_EXCEPTION);
-                $this->pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, true);
+                if (DEBUG) {
+                    $this->pdo->setAttribute(PDO::ATTR_EMULATE, PDO::ERRMODE_EXCEPTION);
+                    $this->pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, true);
+                }
+            } catch (\PDOException $e) {
+                throw new Exception('Could not connect to database');
             }
-        } catch (\PDOException $e) {
-            throw new Exception('Could not connect to database');
         }
     }
 
