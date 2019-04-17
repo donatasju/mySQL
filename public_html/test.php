14e163d663ac8dc4a7b2059c4a485e746d6f28af
donatasju <donatas.jurkusl@gmail.com>
2019-04-17 10:00:43 +0300

    done

--- public_html/test.php
+++ public_html/test.php
@@ -1,5 +1,4 @@
 <?php
-
 require_once '../bootloader.php';
 
 $db = new \Core\Database\Connection([
@@ -9,24 +8,50 @@ $db = new \Core\Database\Connection([
         ]);
 
 $pdo = $db->getPDO();
-$query = $pdo->prepare("INSERT INTO `my_db`.`users`"
-        . "(`email`, `password`, `full_name`, `age`, `gender`, `photo`)" .
-        "VALUES(:email, :password, :full_name, :age, :gender, :photo)");
+//$query = $pdo->prepare("INSERT INTO `my_db`.`users`"
+//        . "(`email`, `password`, `full_name`, `age`, `gender`, `photo`)" .
+//        "VALUES(:email, :password, :full_name, :age, :gender, :photo)");
+//
+//$data = [
+//    'email' => 'augis@gmail.com',
+//    'password' => 'deasfds',
+//    'full_name' => 'augis papa',
+//    'age' => 2,
+//    'gender' => 'm',
+//    'photo' => 'augis.jpg'
+//];
+//
+//foreach ($data as $key => &$value) {
+//    $query->bindParam(':' . $key, $value);
+//}
+//
+//$query->execute();
 
-$data = [
-    'email' => 'augis@gmail.com',
-    'password' => 'deasfds',
-    'full_name' => 'augis papa',
-    'age' => 2,
-    'gender' => 'm',
-    'photo' => 'augis.jpg'
-];
+$query = $pdo->query("SELECT * FROM `my_db`.`users`");
+$data = $query->fetchAll(PDO::FETCH_ASSOC);
+?>
 
-foreach ($data as $key => &$value) {
-    $query->bindParam(':' . $key, $value);
-}
+<html>
+    <head>
+        <title>DB</title>
+    </head>
+    <body>
+        <?php foreach ($data as $row): ?>
+            <ul>
+                <li>Email: <?php print $row['email']; ?></li>
+                <li>Password: <?php print $row['password']; ?></li>
+                <li>Full Name: <?php print $row['full_name']; ?></li>
+                <li>Age: <?php print $row['age']; ?></li>
+                <li>Gender: <?php print $row['gender']; ?></li>
+                <li>Created at: <?php print $row['created_at']; ?></li>
+                <li>Updated at: <?php print $row['updated_at']; ?></li>
+            </ul>
+        <?php endforeach; ?>
+    </body>
+</html>
 
-$query->execute();
+
+
 
 
 
